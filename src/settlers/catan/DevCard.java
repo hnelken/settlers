40955d4166991044c236acee0a925483321ca6f4
nfleclerc <nfleclerc@live.com>
2015-02-07 02:49:47 -0500

    new devcard
    Signed-off-by: nfleclerc <nfleclerc@live.com>

--- src/settlers/catan/DevCard.java
+++ src/settlers/catan/DevCard.java
@@ -1,15 +1,40 @@
 package settlers.catan;
 
-public abstract class DevCard {
+public class DevCard {
 	
 	// fields
 	private Player owner;
 	private boolean playable;
+	private String name;
+	
+	public static enum Type {
+		TROOPER, VP, SANDSTORM, SANDCRAWLER, BLUEHARVEST
+	}
+	
+	private Type type;
 	
 	// constructors
 	
-	public DevCard() {
+	public DevCard(Type type) {
 		setOwner(null);
+		if (type == Type.TROOPER){
+			this.name = "Stormtrooper";
+		} else if (type == Type.SANDSTORM){
+			this.name = "Sandstorm";
+		} else if (type == Type.SANDCRAWLER){
+			this.name = "Sandcrawler";
+		} else if (type == Type.BLUEHARVEST){
+			this.name = "Blue Harvest";
+		}
+	}
+	
+	public DevCard(String name){
+		this.name = name;
+		this.type = Type.VP;
+	}
+	
+	public void play(){
+		
 	}
 
 	public Player getOwner() {
@@ -18,6 +43,14 @@ public abstract class DevCard {
 
 	public void setOwner(Player owner) {
 		this.owner = owner;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
 	}
 	
 }
