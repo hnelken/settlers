7782c6c610ac5ee8d395bddc857f11e8b2920444
Harry Nelken <hrn10@case.edu>
2015-02-07 08:49:21 -0500

    hell
    Signed-off-by: Harry Nelken <hrn10@case.edu>

--- src/settlers/catan/DevCard.java
+++ src/settlers/catan/DevCard.java
@@ -1,191 +1,189 @@
-<<<<<<< HEAD
+/*<<<<<<< HEAD
 package settlers.catan;
 
 public class DevCard {
- 
- // fields
- private Player owner;
- private boolean playable;
- private String name;
- private String description;
- 
- public enum Type {
-  TROOPER, VP, SANDSTORM, SANDCRAWLER, BLUEHARVEST
- }
- 
- private Type type;
- 
- // constructors
- 
- public DevCard(Type type) {
-  this.owner = null;
-  if (type == Type.TROOPER){
-   this.setName("Stormtrooper");
-   this.description = "Move the robber. Steal 1 resource card from the owner of an adjacent settlement or city.";
-  }
-  else if (type == Type.SANDSTORM){
-   this.setName("Sandstorm");
-   this.description = "When you play this card, you can select two resources of your choice from the bank.";
-  }
-  else if (type == Type.SANDCRAWLER){
-   this.setName("Sandcrawler");
-   this.description = "Place two new roads as if you had just built them.";
-  }
-  else if (type == Type.BLUEHARVEST){
-   this.setName("Blue Harvest");
-   this.description = "When you play this card, you can select 2 resources of your choice from the bank.";
-  }
-  this.setPlayable(false);
- }
- 
- public DevCard(String name){
-  this.setName(name);
-  this.type = Type.VP;
-  this.description = "1 Victory Point!";
-  this.owner = null;
- }
- 
- public void play(){
-  if (type == Type.TROOPER){
-	  trooperPlay();
-  }
-  else if (type == Type.SANDSTORM){
-  }
-  else if (type == Type.SANDCRAWLER){
-  }
-  else if (type == Type.BLUEHARVEST){
-  }
-  else if (type == Type.VP){
-  }
- }
- 
- private void trooperPlay(){
-   
- }
 
- public Player getOwner() {
-  return owner;
- }
+	// fields
+	private Player owner;
+	private boolean playable;
+	private String name;
+	private String description;
 
- public void setOwner(Player owner) {
-  this.owner = owner;
- }
-
- public Type getType() {
-  return type;
- }
-
- public void setType(Type type) {
-  this.type = type;
- }
- 
- public boolean isPlayable() {
-  return playable;
- }
- 
- public void setPlayable(boolean playable) {
-  this.playable = playable;
- }
-
- public String getName() {
-  return name;
- }
-
- public void setName(String name) {
-  this.name = name;
- }
-}
-=======
-	package settlers.catan;
-	
-	public class DevCard {
-	 
-	 // fields
-	 private Player owner;
-	 private boolean playable;
-	 private String name;
-	 private String description;
-	 
-	 public enum Type {
-	  TROOPER, VP, SANDSTORM, SANDCRAWLER, BLUEHARVEST
-	 }
-	 
-	 private Type type;
-	 
-	 // constructors
-	 
-	 public DevCard(Type type) {
-	  this.owner = null;
-	  if (type == Type.TROOPER){
-	   this.setName("Stormtrooper");
-	   this.setDescription("Move the robber. Steal 1 resource card from the owner of an adjacent settlement or city.");
-	  }
-	  else if (type == Type.SANDSTORM){
-	   this.setName("Sandstorm");
-	   this.setDescription("When you play this card, you can select two resources of your choice from the bank.");
-	   this.description = "Move the robber. Steal 1 resource card from the owner of an adjacent settlement or city.";
-	  }
-	  else if (type == Type.SANDSTORM){
-	   this.setName("Sandstorm");
-	   this.description = "When you play this card, you can select two resources of your choice from the bank.";
-	  }
-	  else if (type == Type.SANDCRAWLER){
-	   this.setName("Sandcrawler");
-	   this.setDescription("Place two new roads as if you had just built them.");
-	  }
-	  else if (type == Type.BLUEHARVEST){
-	   this.setName("Blue Harvest");
-	   this.setDescription("When you play this card, you can select 2 resources of your choice from the bank.");
-	  }
-	  this.setPlayable(false);
-	 }
-	
-	public DevCard(String name){
-	  this.setName(name);
-	  this.type = Type.VP;
-	  this.setDescription("1 Victory Point!");
-	  this.owner = null;
-	 }
-	
-	 public Player getOwner() {
-	  return owner;
-	 }
-	
-	 public void setOwner(Player owner) {
-	  this.owner = owner;
-	 }
-	
-	 public Type getType() {
-	  return type;
-	 }
-	
-	 public void setType(Type type) {
-	  this.type = type;
-	 }
-	 
-	 public boolean isPlayable() {
-	  return playable;
-	 }
-	
-	 public void setPlayable(boolean playable) {
-	  this.playable = playable;
-	 }
-	
-	 public String getName() {
-	  return name;
-	 }
-	
-	public String getDescription() {
-		return description;
+	public enum Type {
+		TROOPER, VP, SANDSTORM, SANDCRAWLER, BLUEHARVEST
 	}
-	
+
+	private Type type;
+
+	// constructors
+
+	public DevCard(Type type) {
+		this.owner = null;
+		if (type == Type.TROOPER){
+			this.setName("Stormtrooper");
+			this.description = "Move the robber. Steal 1 resource card from the owner of an adjacent settlement or city.";
+		}
+		else if (type == Type.SANDSTORM){
+			this.setName("Sandstorm");
+			this.description = "When you play this card, you can select two resources of your choice from the bank.";
+		}
+		else if (type == Type.SANDCRAWLER){
+			this.setName("Sandcrawler");
+			this.description = "Place two new roads as if you had just built them.";
+		}
+		else if (type == Type.BLUEHARVEST){
+			this.setName("Blue Harvest");
+			this.description = "When you play this card, you can select 2 resources of your choice from the bank.";
+		}
+		this.setPlayable(false);
+	}
+
+	public DevCard(String name){
+		this.setName(name);
+		this.type = Type.VP;
+		this.description = "1 Victory Point!";
+		this.owner = null;
+	}
+
+	public void play(){
+		if (type == Type.TROOPER){
+			trooperPlay();
+		}
+		else if (type == Type.SANDSTORM){
+		}
+		else if (type == Type.SANDCRAWLER){
+		}
+		else if (type == Type.BLUEHARVEST){
+		}
+		else if (type == Type.VP){
+		}
+	}
+
+	private void trooperPlay(){
+
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public boolean isPlayable() {
+		return playable;
+	}
+
+	public void setPlayable(boolean playable) {
+		this.playable = playable;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+} */
+package settlers.catan;
+
+public class DevCard {
+
+	// fields
+	private Player owner;
+	private boolean playable;
+	private String name;
+	private String description;
+
+	public enum Type {
+		TROOPER, VP, SANDSTORM, SANDCRAWLER, BLUEHARVEST
+	}
+
+	private Type type;
+
+	// constructors
+
+	public DevCard(Type type) {
+		this.owner = null;
+		if (type == Type.TROOPER){
+			this.setName("Stormtrooper");
+			this.setDescription("Move the robber. Steal 1 resource card from the owner of an adjacent settlement or city.");
+		}
+		else if (type == Type.SANDSTORM){
+			this.setName("Sandstorm");
+			this.setDescription("When you play this card, you can select two resources of your choice from the bank.");
+			this.description = "Move the robber. Steal 1 resource card from the owner of an adjacent settlement or city.";
+		}
+		else if (type == Type.SANDSTORM){
+			this.setName("Sandstorm");
+			this.description = "When you play this card, you can select two resources of your choice from the bank.";
+		}
+		else if (type == Type.SANDCRAWLER){
+			this.setName("Sandcrawler");
+			this.setDescription("Place two new roads as if you had just built them.");
+		}
+		else if (type == Type.BLUEHARVEST){
+			this.setName("Blue Harvest");
+			this.setDescription("When you play this card, you can select 2 resources of your choice from the bank.");
+		}
+		this.setPlayable(false);
+	}
+
+	public DevCard(String name){
+		this.setName(name);
+		this.type = Type.VP;
+		this.setDescription("1 Victory Point!");
+		this.owner = null;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public boolean isPlayable() {
+		return playable;
+	}
+
+	public void setPlayable(boolean playable) {
+		this.playable = playable;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	}
->>>>>>> 60fda28673a3592eea910d0055dbbddc17c58d85
+
+}
