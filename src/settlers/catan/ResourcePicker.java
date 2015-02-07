cd6ebf554eb56f8f6d0500b3207178d303da3c5d
teddybear02 <theo.frohlich@gmail.com>
2015-02-07 10:33:25 -0500

    just a bit
    Signed-off-by: teddybear02 <theo.frohlich@gmail.com>

--- src/settlers/catan/ResourcePicker.java
+++ src/settlers/catan/ResourcePicker.java
@@ -5,16 +5,11 @@ import BreezyGUI.*;
 
 public class ResourcePicker extends GBFrame {
 	
-<<<<<<< HEAD
-=======
-	private static final long serialVersionUID = 1L;
->>>>>>> 554081401e51479aab1b406dab89db0f26fe5d78
 	// fields
 	private static final long serialVersionUID = 1L;
-	private Button[] buttons = new Button[5];
+	private Button[] resourceButtons = new Button[5];
 
-	// constructors
-
+	// constructor
 	public ResourcePicker() {
 		
 	}
