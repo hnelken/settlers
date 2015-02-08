740d92a83773bf78bb2f5d4add5a3d8995564fb8
Harry Nelken <hrn10@case.edu>
2015-02-07 22:50:11 -0500

    main file additions
    Signed-off-by: Harry Nelken <hrn10@case.edu>

--- src/settlers/catan/Main.java
+++ src/settlers/catan/Main.java
@@ -3,7 +3,18 @@ package settlers.catan;
 public class Main {
 
 	public static void main(String[] args) {
+		if (args.length != 3 && args.length != 4) {
+			System.err.println("Invalid number of players!");
+			System.exit(-1);
+		}
 		
+		Player[] players = new Player[args.length];
+		for (int i = 0; i < args.length; i++) {
+			players[i] = new Player(args[i], i + 1);
+			System.out.println("Player " + (i + 1) + " is " + args[i] + "!");
+		}
+		
+		GameManager manager = new GameManager(players);
 	}
 
 }
